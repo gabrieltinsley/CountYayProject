5df867938db861a31fb7f93a523f3ca91cf0abe9
Gabriel Tinsley <gabrieltinsley@u.boisestate.edu>
2024-04-28 13:12:03 -0600

    CountYay is complete

--- CountYay.java
+++ CountYay.java
@@ -1,1 +1,14 @@
-
+public class CountYay {
+    public static void main(String[] args) {
+        for(int i = 100; i > 0; i--) {
+            if(i % 3 == 0) {
+                if(i % 3 == 0 && i % 11 == 0) {
+                    System.out.print("Yay! ");
+                } else {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+        System.out.println();
+    }
+}
